4902c99f7e1d89860289059941cc0f4cbebd14ab
EC2 Default User <ec2-user@ip-172-31-31-217.ec2.internal>
2018-09-05 23:57:03 +0000

    Moved files

---
+++ first_cell.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+plt.style.use('fivethirtyeight')
+plt.rcParams['font.size'] = 18
+
+import seaborn as sns
+
+%load_ext autoreload
+%autoreload 2
